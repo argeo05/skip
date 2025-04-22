8b8781b435ddff7e2cf9329b7d0ba7d881edb85c
GeorgiyAbramov <argeo05@mail.ru>
2025-04-22 20:15:40 +0300

    [ADD] Introduce Return instruction to handle function termination.

---
+++ cvm.core/src/main/java/cvm/instructions/special/Return.java
@@ -0,0 +1,29 @@
+package cvm.instructions.special;
+
+import cvm.Context;
+import cvm.instructions.AbstractVmInstruction;
+
+/**
+ * <b>Return</b> Instruction. Pops one value and завершает текущую функцию.
+ * @bytecode ret
+ * @opcode 20
+ */
+public final class Return extends AbstractVmInstruction {
+    private final Context ctx;
+    private long value;
+
+    public Return(byte type, Context ctx) {
+        super(type);
+        this.ctx = ctx;
+    }
+
+    @Override
+    protected void popArgs() {
+        value = ctx.pop();
+    }
+
+    @Override
+    protected void run() {
+        ctx.returnFromFunction(value);
+    }
+}
